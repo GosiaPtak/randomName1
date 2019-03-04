3a8a6fdf56c55204405a878e77852a4e57a2e503
GosiaPtak <ptakma@gmail.com>
2019-03-04 19:11:44 +0100

    added assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import randomeNames from './assets/randomeNames.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'randomName1';
