0ed6b47ba6afb789fc1809330d4db0960fe84ca9
Malgorzata <ptakma@gmail.com>
2019-03-15 18:30:20 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-import randomeNames from './assets/randomeNames.json';
+import { Component, OnInit } from '@angular/core';
+
+import randomNames from '../assets/randomNames.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'randomName1';
+export class AppComponent implements OnInit {
+  title = 'Losowe Imie';
+  public randomName: string;
+  constructor() {}
+
+  ngOnInit() {
+    this.randomName =
+      randomNames[Math.floor(Math.random() * randomNames.length)];
+  }
 }
